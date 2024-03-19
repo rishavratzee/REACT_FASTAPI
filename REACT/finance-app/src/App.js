8aadbbb56f0ff473a040f71803c65bbefaec1730
rishavratzee <rishavforu0123@gmail.com>
2024-03-20 00:35:36 +0530

    React Code Final

--- REACT/finance-app/src/App.js
+++ REACT/finance-app/src/App.js
@@ -7,7 +7,7 @@ const App = ()=>{
     amount:'',
     category:'',
     description:'',
-    is_income:'',
+    is_income:false,
     date:''
   });
 
@@ -37,7 +37,7 @@ const App = ()=>{
       amount:'',
     category:'',
     description:'',
-    is_income:'',
+    is_income:false,
     date:''
     });
   };
@@ -50,8 +50,74 @@ const App = ()=>{
             Finance App
           </a>
         </div>
-
       </nav>
+      
+      <div className='container'>
+        <form onSubmit={handleFormSubmit}>
+          <div className='mb-3 mt-3'>
+            <label htmlFor='amount' className='form-label'>
+              Amount
+            </label>
+            <input type='text' className='form-control' id='amount' name='amount' onChange={handleInputChange} value={formData.amount} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='category' className='form-label'>
+              Category
+            </label>
+            <input type='text' className='form-control' id='category' name='category' onChange={handleInputChange} value={formData.category} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='description' className='form-label'>
+              Description
+            </label>
+            <input type='text' className='form-control' id='description' name='description' onChange={handleInputChange} value={formData.description} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='is_income' className='form-label'>
+              Income?
+            </label>
+            <input type='checkbox'  id='description' name='is_income' onChange={handleInputChange} value={formData.is_income} />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='date' className='form-label'>
+              Date
+            </label>
+            <input type='text' className='form-control' id='date' name='date' onChange={handleInputChange} value={formData.date} />
+          </div>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
+
+        <table className='table table-striped table-bordered table-hover'>
+          <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Income</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction)=>(
+              <tr key={transaction.id}>
+                <td>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.is_income ? 'Yes' : 'No'}</td>
+                <td>{transaction.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
+
     </div>
   )
 
